eadafc8007c86e5d9522a6c2c6847cda7a878067
Mohammed Swalih N K <swalink555@gmail.com>
2026-02-20 22:13:35 +0530

    fix: index.py for collectstatic

--- api/index.py
+++ api/index.py
@@ -1,8 +1,12 @@
-import os
-import sys
-
+import os, sys
 sys.path.append(os.path.dirname(os.path.dirname(__file__)))
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "student_management.settings")
+
+import django
+django.setup()
+
+from django.core.management import call_command
+call_command('collectstatic', '--noinput', verbosity=0)
 
 from student_management.wsgi import application
 app = application
