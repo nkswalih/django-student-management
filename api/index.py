6da17e9e42259f6e850dfc9e65c510f2d79464e6
Mohammed Swalih N K <swalink555@gmail.com>
2026-02-26 21:11:54 +0530

    fix: staticfiles vercel

--- api/index.py
+++
@@ -1,8 +0,0 @@
-import os
-import sys
-
-sys.path.append(os.path.dirname(os.path.dirname(__file__)))
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "student_management.settings")
-
-from student_management.wsgi import application
-app = application
--- student_management/settings.py
+++ student_management/settings.py
@@ -140,8 +140,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-STATICFILES_STORAGE = 'whitenoise.storage.StaticFilesStorage'
+STATIC_ROOT = BASE_DIR / "staticfiles"
+STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media') 
