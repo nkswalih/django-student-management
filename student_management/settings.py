7df0a2dd46c8e2196ba9b7e764380ab0edd8597f
Mohammed Swalih N K <swalink555@gmail.com>
2026-03-07 12:44:12 +0530

    feat: added favicon

--- student_management/settings.py
+++ student_management/settings.py
@@ -26,7 +26,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/6.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get('SECRET_KEY')
+SECRET_KEY = os.environ.get('SECRET_KEY', "dev-secret-key")
 SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
 
 # SECURITY WARNING: don't run with debug turned on in production!
